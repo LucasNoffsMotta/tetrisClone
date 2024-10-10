34b2038569b90531fea15cf741e955d434e2bc5e
Lucas Motta <lnoffs@hotmail.com>
2024-10-09 19:21:29 -0700

    Update

---
+++ PositionsData.cs
@@ -0,0 +1,28 @@
+﻿//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Graphics;
+//using SharpDX.MediaFoundation.DirectX;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Tetris
+//{
+//    public static class CreateObjects
+//    {
+//        public static Dictionary<string, List<Point>> rotationValues = new Dictionary<string, List<Point>>();
+        
+//        public static void ObjectsFactory(string typeOfObject, string textureFile, List<Brick>emptyList)
+//        {
+//            for (int i = 0; i < 4; i++)
+//            {
+//                emptyList.Add(new(Globals.Content.Load<Texture2D>(textureFile), )
+//            }
+            
+
+//        }
+
+        
+//    }
+//}
--- obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458d21e1482329dba924e0b0a00cf913ce98c4af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827c9aabc6f7c6f2687dd71f8209103f8d081dbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
