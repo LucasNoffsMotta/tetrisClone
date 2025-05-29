ad2cca8281d204ad54c057286fcc3c7ba7b71512
Lucas Motta <lnoffs@hotmail.com>
2025-05-29 17:22:49 -0300

    Arquivos locais

--- obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554671ebc045b16ddf0723142947e7e9e4ab77b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+330536f927ad0aa39d3b7d56cee166c39f725e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
